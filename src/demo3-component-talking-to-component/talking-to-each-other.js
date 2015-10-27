53c7121ee1dc480a1e9c7a01faee61877bd600b3
Think <liaosong1015@gmail.com>
2015-10-27 16:14:06 +0800

    finish demo3

---
+++ src/demo3-component-talking-to-component/talking-to-each-other.js
@@ -0,0 +1,92 @@
+//- CommentBox
+//  - CommentList
+//    - Comment
+// - CommentForm
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var data = [
+  {author: "Pete Hunt", content: "this post is very nice.", id: "1"},
+  {author: "Jordan Walke", content: "nice article", id: "2"}
+];
+
+class CommentBox extends React.Component {
+  commentSubmitHandle(comment){
+    if(!comment.id){
+      comment.id = this.props.data.length+1;
+    }
+
+    let newData = this.props.data.push(comment);
+    this.setState({data: newData});
+  }
+
+  render() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.props.data}/>
+        <CommentForm onCommentSubmit={this.commentSubmitHandle.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+class CommentList extends React.Component {
+  render() {
+    var commnets = this.props.data.map(function (comment) {
+      return (
+        <Comment data={comment} key={comment.id}/>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commnets}
+      </div>
+    );
+  }
+}
+
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="commnet">
+        <span>{this.props.data.author}:</span>
+
+        <span>{this.props.data.content}</span>
+      </div>
+    );
+  }
+}
+
+class CommentForm extends React.Component {
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(arguments);
+    let author = this.refs.author.value.trim();
+    let content = this.refs.content.value.trim();
+    if(!(author && content)) return;
+
+    this.props.onCommentSubmit({author: author, content: content});
+    this.refs.author.value = '';
+    this.refs.content.value = '';
+    return;
+
+  }
+  render() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit.bind(this)}>
+        <label>name:</label>
+        <input placeholder="input author name" ref="author"/>
+        <br/>
+        <label>content</label>
+        <textarea placeholder="please input your  point!" ref="content"></textarea>
+        <br/>
+        <button type="submit">submit</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<CommentBox   data={data}/>, document.getElementById('content'));
