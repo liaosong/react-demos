a91f0865911bdbba03b45b810bfd80e8e1897179
Think <liaosong1015@gmail.com>
2015-10-27 10:29:48 +0800

    finish demo2

---
+++ src/demo2-nested-component/nested-component.js
@@ -0,0 +1,41 @@
+//- CommentBox
+//  - CommentList
+//    - Comment
+// - CommentForm
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+}
+
+class CommentList extends React.Component {
+  render() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+}
+
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<CommentBox  />, document.getElementById('content'));
